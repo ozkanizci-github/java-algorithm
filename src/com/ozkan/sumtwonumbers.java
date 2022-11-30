085b12d4d7dcc45b9ff6645258becdc8ad31bac5
özkan <46302195+Cooddering@users.noreply.github.com>
2022-11-30 09:43:51 +0300

    two commit

--- src/com/ozkan/sumtwonumbers.java
+++ src/com/ozkan/sumtwonumbers.java
@@ -2,9 +2,9 @@ package com.ozkan;
 
 public class sumtwonumbers{
         public static void main(String[] args) {
-            int num1 = 3;
-            int num2 = 4;
-            int result = num1 + num2;
+            double num1 = 3;
+            double num2 = 4;
+            double result = num1 + num2;
 
             System.out.println("Sum of two numbers : " + result);
         }
