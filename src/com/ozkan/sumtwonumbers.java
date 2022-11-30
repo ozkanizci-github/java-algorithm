7415217062f6676ca1848d09122c72443f78e66b
özkan <46302195+Cooddering@users.noreply.github.com>
2022-11-30 09:41:12 +0300

    test commit

---
+++ src/com/ozkan/sumtwonumbers.java
@@ -0,0 +1,12 @@
+package com.ozkan;
+
+public class sumtwonumbers{
+        public static void main(String[] args) {
+            int num1 = 3;
+            int num2 = 4;
+            int result = num1 + num2;
+
+            System.out.println("Sum of two numbers : " + result);
+        }
+}
+
